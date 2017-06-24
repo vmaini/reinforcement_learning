8384fe61db19543c518ead94fc425d910e76c53e
vishal <vishal.maini@gmail.com>
2017-06-24 16:05:53 -0700

    add data viz

--- q_table_learning.py
+++ q_table_learning.py
@@ -1,4 +1,4 @@
-# Guided by https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+# Source: https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
 
 import gym
 import numpy as np
@@ -28,22 +28,26 @@ for i in range(num_episodes):
     while j < 99:
         j += 1
         # greedily pick the best action (column) given the state (row), add noise
-        a = np.argmax(Q[s,:]) + np.random.randn(1,num_actions)*(1./(i+1)) #action in Q(s,a)
+
+        a = np.argmax(Q[s,:] + np.random.randn(1,num_actions)*(1./(i+1)))
+         #action in Q(s,a)
 
         s_prime, reward, done, info = env.step(a)
 
         # update Q-table to incorporate results of previous action (add diff between Q(s',a) and Q(s,a) modulated by learning rate)
         Q[s,a] = Q[s,a] + learning_rate * (reward + discount_rate * np.max(Q[s_prime,:]) - Q[s,a])
         reward_sum += reward
-        s = s1
+        s = s_prime
 
         if done:
             break
-        rewards_list.append(reward_sum)
+    rewards_list.append(reward_sum)
 
-t = np.arange(0,num_episodes,1)
-plt.plot(t,rewards_list,'b')
+# t = np.arange(0,num_episodes,1)
+# plt.plot(t,rewards_list,'b')
+plt.hist(rewards_list)
 plt.show()
 
+print 'average reward: ' + str(sum(rewards_list)/num_episodes)
 print 'final Q-table values:'
 print Q
